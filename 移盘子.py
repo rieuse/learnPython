b834ae71c11ec18562da8348d67e08d43ee06bd7
LU-TAISHI2\Shinelon <rieuse@163.com>
2017-04-07 14:25:10 +0800

    ok

--- 移盘子.py
+++ 移盘子.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 def move(n,a,b,c,):
+
     if n==1:
         print(a,'-->',c)
     else:
         move(n-1,a,c,b)
         move(1,a,b,c)
         move(n-1,b,a,c)
-move(3,'A','B','C')
-
-
+    global num
+    num = 2*n + 1
+move(5,'A','B','C')
+print(num)
