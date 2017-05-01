2c7f2cb0c493bc0807fc430040f4cb4ca2df3e4c
LU-TAISHI2\Shinelon <rieuse@163.com>
2017-05-01 16:42:41 +0800

    添加

--- 技巧-csv的使用.py
+++ 技巧-csv的使用.py
@@ -8,11 +8,11 @@ import csv
 
 
 # 使用csv模块操作 csv文件，输出内容
-# with open('file/sample.csv','w') as file:
+with open('file/sample.csv','a') as file:
     # reader = csv.reader(file)
     # for title, year, director in reader:
     #     print(title, year, director)
-with open('file/sample.csv','a') as file:
+
     writer = csv.writer(file)
-    writer.writerows(['title', 'summary', 'year'])
-    writer.writerow(['文章标题', '作者', '时间', '阅读量', '评论', '喜欢', '赞赏数', '文章地址'])
+    writer.writerow(['title', 'summary', 'year'])
+
