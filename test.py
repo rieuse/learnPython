f60acc6e3570c902a254461356a7ac25c5a54eaf
LU-TAISHI2\Shinelon <rieuse@163.com>
2017-05-06 22:16:04 +0800

    修改

--- test.py
+++ test.py
@@ -1,13 +1,8 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
-import time
 
-driver = webdriver.PhantomJS(executable_path=r'D:\phantomjs-2.1.1-windows\bin\phantomjs')
-driver.get('http://www.toutiao.com/ch/news_tech/')
-time.sleep(3)
-soup = BeautifulSoup(driver.page_source, 'lxml')
-articles = []
-for article in soup.find_all(class_='item', ga_event="article_item_click"):
-    tags = article.get_text()
-    print(article)
-    # print(tags)
+driver = webdriver.Firefox()
+driver.get('http://bbs.myquark.cn/')
+cookie = driver.get_cookies()
+driver.quit()
+print(cookie)
