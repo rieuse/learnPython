bbf9ea7a1dc0e6af14895498f9804e822d427cca
LU-TAISHI2\Shinelon <rieuse@163.com>
2017-05-07 15:47:36 +0800

    增加装饰器的例子

--- Python基础示例代码/装饰器示例.py
+++ Python基础示例代码/装饰器示例.py
@@ -33,17 +33,14 @@ def outer(function):
 
 def wait_for_deco():
     return 1024
-
-
 decorated = outer(wait_for_deco)
 print(decorated())
 
 
+# 使用装饰器
 @outer
 def wait_for_deco():
     return 1024
-
-
 print(wait_for_deco())
 
 
@@ -59,6 +56,49 @@ def decorator(func):
 @decorator
 def wait_for_deco(x, y):
     return x + y
-
-
 print(wait_for_deco(1, 2))
+
+
+# 原函数有x, y, z三个参数，把结果放大两倍的装饰器
+def decorator(func):
+    def wrapper(*args, **kwargs):
+        ret = func(*args, **kwargs)
+        return ret * 2
+
+    return wrapper
+
+
+@decorator
+def wait_for_deco_a(x, y):
+    return x + y
+
+
+@decorator
+def wait_for_deco_b(x, y, z):
+    return x + y + z
+
+
+print(wait_for_deco_a(1, 2))
+print(wait_for_deco_b(1, 2, 3))
+
+
+# decrator(args)返回的是最终需要的装饰器就好了。所以，带参数的装饰器就需要写成下面这样：
+def decorator(name):
+    print("在这里使用装饰器的name参数：", name)
+
+    def wrapper(func):
+        print("在这里也可用装饰器的name参数：", name)
+
+        def _wrapper(*args, **kwargs):
+            print("这里还可使用装饰器的name参数：", name)
+            ret = func(*args, **kwargs)  # 这里进行原函数的计算
+            return ret * 2
+
+        return _wrapper  # 返回可调用对象，_wrapper可以接受原函数的参数
+
+    return wrapper  # 返回真正的装饰器，接受原函数作为第一个参数
+
+
+@decorator('haha')
+def wait_for_deco(x, y):
+    return x + y
