c5758a78977bcadd92809fc81da8fcc2f2fa7758
LU-TAISHI2\Shinelon <rieuse@163.com>
2017-05-16 20:54:45 +0800

    增加算法

---
+++ Python算法/冒泡排序.py
@@ -0,0 +1,17 @@
+'''
+它重复地走访过要排序的数列，一次比较两个元素，如果他们的顺序错误就把他们交换过来。
+走访数列的工作是重复地进行直到没有再需要交换，也就是说该数列已经排序完成。
+'''
+
+
+def bubble_sort(lists):
+    count = len(lists)
+    for i in range(0, count):
+        for j in range(i + 1, count):
+            if lists[i] > lists[j]:
+                lists[i], lists[j] = lists[j], lists[i]
+    return lists
+
+
+arr = [2, 645, 1, 344, 546, 442, 89, 99, 76, 90]
+print(bubble_sort(arr))
--- test.py
+++ test.py
@@ -1,21 +1,10 @@
-def BinarySearch(arr, key):
-    min = 0
-    max = len(arr) - 1
-    if key in arr:
-        while True:
-            # 这里一定要加int或者使用整除 //，防止列表是偶数的时候出现浮点数据int((min + max) / 2)
-            mid = (min + max) // 2
-            if arr[mid] > key:
-                max = mid - 1
-            elif arr[mid] < key:
-                min = mid + 1
-            elif arr[mid] == key:
-                print(str(key) + "在数组里面的第" + str(mid) + "个位置")
-                return arr[mid]
-    else:
-        print("没有该数字!")
-
-
-arr = [1, 6, 9, 15, 26, 38, 49, 57, 63, 77, 81, 93]
-key = input("请输入你要查找的数字：")
-BinarySearch(arr, int(key))
+def insert_sort(list):
+    length = len(list)
+    for i in range(1, length):
+        key = list[i]
+        j = i - 1
+        while j >= 0:
+            if list[j] > key:
+                list[j + 1], list[j] = list[j], key
+            j -= 1
+    return list
