6a1e4ff2142fb045b6e4be662d064dfcc923db6c
LU-TAISHI2\Shinelon <rieuse@163.com>
2017-05-17 14:12:12 +0800

    增加算法

--- test2.py
+++ test2.py
@@ -1,2 +1,25 @@
-def insert_sort(list):
-    length = len(list)
+def ShellInsetSort(array, len_array, dk):  # 直接插入排序
+    for i in range(dk, len_array):  # 从下标为dk的数进行插入排序
+        position = i
+        current_val = array[position]  # 要插入的数
+        index = i
+        j = int(index / dk)  # index与dk的商
+        index = index - j * dk
+        while position > index and current_val < array[position - dk]:
+            array[position] = array[position - dk]  # 往后移动
+            position = position - dk
+        else:
+            array[position] = current_val
+
+
+def ShellSort(array, len_array):  # 希尔排序
+    dk = int(len_array / 2)  # 增量
+    while (dk >= 1):
+        ShellInsetSort(array, len_array, dk)
+        print(">>:", array)
+        dk = int(dk / 2)
+
+
+array = [49, 38, 65, 97, 76, 13, 27, 49, 55, 4]
+print(">:", array)
+ShellSort(array, len(array))
