081bc2e1ac682bb2aa2dd26d6860a0a86f87bcf5
rieuse <rieuse@163.com>
2017-05-22 14:53:28 +0800

    整理

--- test4.py
+++
@@ -1,5 +0,0 @@
-dict1 = [1, 34, 4, 5, 6]
-dict2 = [4, 4, 6, 88]
-dict1 += dict2
-print(dict1)
-# print(dict1.extend(dict2))
--- 技巧-使用代理请求网页.py
+++ 技巧-使用代理请求网页.py
@@ -1,9 +1,16 @@
 import requests
 
 proxies = {
-    "http": "http://123.206.6.17:90",
-    "https": "http://123.206.6.17:90",
+    # "http": "http://218.201.98.196:3128",
+    "https": "http://218.201.98.196:3128",
 }
-r = requests.get("http://httpbin.org/ip", proxies=proxies)
+r = requests.get("https://httpbin.org/ip", proxies=proxies)
 html = r.text
 print(html)
+
+
+# 代理池  http://7xrnwq.com1.z0.glb.clouddn.com/proxy_list.txt
+# 代理池 http://api.xicidaili.com/free2016.txt
+
+# request = requests.get('http://7xrnwq.com1.z0.glb.clouddn.com/proxy_list.txt')
+# print(request.text)
