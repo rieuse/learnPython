1d56a5c0f40fc3e3a96089f67265bbca2ab0b147
rieuse <rieuse@163.com>
2017-06-02 13:09:17 +0800

    斗鱼

--- .idea/matplotlib示例1.py
+++
@@ -1,7 +0,0 @@
-import matplotlib.pyplot as plt
-
-plt.rcParams['font.sans-serif'] = ['SimHei']  # 用来正常显示中文标签
-plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
-
-plt.bar(left=(0, 1), height=(1, 0.5), width=0.35)
-plt.show()
