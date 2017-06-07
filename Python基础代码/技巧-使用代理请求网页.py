7f217af32306c4ba11ca59be5b21d12f0158da9c
rieuse <rieuse@163.com>
2017-06-07 13:55:19 +0800

    proxy add htpps

--- Python基础代码/技巧-使用代理请求网页.py
+++ Python基础代码/技巧-使用代理请求网页.py
@@ -17,8 +17,6 @@ from bs4 import BeautifulSoup
 
 
 ss = requests.session()
-
 ss.proxies = {'http': 'http://123.206.6.17:3128', 'https': 'http://123.206.6.17:3128'}
-
 print(ss.get('http://www.qq.com'))
 print(ss.get('https://www.github.com'))
