fe700b6cbf2eb3125f8986515375895415bc3d89
rieuse <rieuse@163.com>
2017-06-12 10:24:13 +0800

    sort

--- ScrapyDoutu/ScrapyDoutu/settings.py
+++ ScrapyDoutu/ScrapyDoutu/settings.py
@@ -3,6 +3,7 @@ BOT_NAME = 'ScrapyDoutu'
 
 SPIDER_MODULES = ['ScrapyDoutu.spiders']
 NEWSPIDER_MODULE = 'ScrapyDoutu.spiders'
+# 下面设置随机User Agent
 UA_LIST = [
     "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",
     "Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11",
