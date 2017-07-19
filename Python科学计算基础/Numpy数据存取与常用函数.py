bab1db7f252ea5b54ac365fc39302597d8b03167
rieuse <rieuse@163.com>
2017-07-19 22:04:00 +0800

    commit

--- Python科学计算基础/Numpy数据存取与常用函数.py
+++ Python科学计算基础/Numpy数据存取与常用函数.py
@@ -24,5 +24,53 @@ import numpy as np
 
 # b1 = np.loadtxt('a1.csv', delimiter=',')
 # print(b1)
-b2 = np.loadtxt('a2.csv', delimiter=',')
-print(b2)
+# b2 = np.loadtxt('a2.csv', dtype=np.int, delimiter=',')
+# print(b2)
+
+'''CSV只能有效存储一维和二维数组,np.savetxt() np.loadtxt()只能有效存取一维和二维数组'''
+
+"""
+多维数据的存取(字符串与二进制的数据写入读取)
+"""
+# 二进制更节省储存空间 这个和编码有关系
+
+# a.tofile(frame, sep='', format='%s')
+# • frame  : 文件、字符串
+# • sep : 数据分割字符串，如果是空串，写入文件为二进制
+# • format : 写入数据的格式
+
+# a = np.arange(100).reshape(5, 10, 2)
+# a.tofile('b.dat', sep=',', format='%d')
+# a.tofile('c.bat', format='%d')
+
+
+# np.fromfile(frame, dtype=float, count=‐1, sep='')
+# • frame  : 文件、字符串
+# • dtype : 读取的数据类型
+# • count  : 读入元素个数，‐1表示读入整个文件
+# • sep : 数据分割字符串，如果是空串，写入文件为二进制
+
+# a1 = np.fromfile('b.dat', dtype=np.int, sep=',').reshape(5, 10, 2)
+# print(a1)
+# a2 = np.fromfile('c.bat', dtype=np.int).reshape(5, 10, 2)
+# print(a2)
+
+'''
+注意事项：
+该方法需要读取时知道存入文件时数组的维度和元素类型
+a.tofile()和np.fromfile()需要配合使用
+可以通过元数据文件来存储额外信息
+'''
+
+"""
+numpy的便捷文件存取
+"""
+
+# np.save(fname, array) 或 np.savez(fname, array)
+# • fname : 文件名，以.npy为扩展名，压缩扩展名为.npz
+# • array  : 数组变量
+# np.load(fname)
+# • fname : 文件名，以.npy为扩展名，压缩扩展名为.npz
+
+a = np.arange(100).reshape(5, 10, 2)
+np.save('a.npy', a)
