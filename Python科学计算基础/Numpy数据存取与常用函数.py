123f8d89255dce14620dfeebd07914c340d60d66
rieuse <rieuse@163.com>
2017-07-20 06:04:53 +0800

    commit

--- Python科学计算基础/Numpy数据存取与常用函数.py
+++ Python科学计算基础/Numpy数据存取与常用函数.py
@@ -72,5 +72,29 @@ numpy的便捷文件存取
 # np.load(fname)
 # • fname : 文件名，以.npy为扩展名，压缩扩展名为.npz
 
-a = np.arange(100).reshape(5, 10, 2)
-np.save('a.npy', a)
+# a = np.arange(100).reshape(5, 10, 2)
+# np.save('a.npy', a)
+# b = np.load('a.npy')
+# print(b)
+
+"""
+numpy的随机数函数
+"""
+
+# rand(d0,d1,..,dn) 根据d0‐dn创建随机数数组，浮点数，[0,1)，均匀分布
+# randn(d0,d1,..,dn) 根据d0‐dn创建随机数数组，标准正态分布
+# randint(low[,high,shape]) 根据shape创建随机整数或整数数组，范围是[low, high)
+# seed(s) 随机数种子，s是给定的种子值
+
+# a = np.random.rand(3, 4, 5)
+# print(a)
+# b = np.random.randn(3, 4, 5)
+# print(b)
+# c = np.random.randint(100, 800,(3, 4, 2))
+# print(c)
+
+# shuffle(a) 根据数组a的第1轴进行随排列，改变数组x
+# permutation(a) 根据数组a的第1轴产生一个新的乱序数组，不改变数组x
+# choice(a[,size,replace,p])
+# 从一维数组a中以概率p抽取元素，形成size形状新数组
+# replace表示是否可以重用元素，默认为False
