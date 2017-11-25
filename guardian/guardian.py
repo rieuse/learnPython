1411801fbe2e89a8f0fa9027be5a5ca274c62aed
rieuse <rieuse@163.com>
2017-11-25 12:40:13 +0800

    add guardian

--- guardian/guardian.py
+++ guardian/guardian.py
@@ -1,67 +1,71 @@
-import multiprocessing
 import os
-import subprocess
 import psutil
 import time
-
-pwd = os.getcwd()
-all_pid = psutil.pids()
-
-
-def judge_spider():
-    for pid in all_pid:
-        p = psutil.Process(pid)
-        if p.name() == 'python.exe':
-            pid_pwd = p.cwd()
-            if pwd == pid_pwd:
-                print('pid: ' + str(pid))
-                memory_percent = psutil.virtual_memory().percent
-                pid_memory_percent = p.memory_percent()
-                print('memory_percent; ' + str(memory_percent), 'pid_memory_percent: ' + str(pid_memory_percent))
-                # if memory_percent > 30 and pid_memory_percent > 0.17:
-                #     kill_python(pid)
-                #     kill_mongo()
-                #     time.sleep(5)
-                #     print('restart')
-                #     start_mongo()
-                #     start_spider()
-                #     judge_spider()
-
+import sys
 
 def start_spider():
-    os.system('python spider.py')
-    print(os.getpid())
+    os.popen('python spider.py')
+    print('start spider succeed')
 
 
 def start_mongo():
-    os.system('mongod --dbpath=F:\data\db')
-    print('成功开启 mongodb')
+    os.popen('mongod --dbpath=F:\data\db')
+    print('start mongod succeed')
 
 
-def kill_python(pid):
-    os.popen('taskkill.exe /pid:' + str(pid) + ' -f')
+def kill_spider(pid):
+    os.popen('taskkill /pid {} /f'.format(str(pid)))
+    print('kill python over')
 
 
 def kill_mongo():
-    for pid in all_pid:
-        p = psutil.Process(pid)
-        if p.name() == 'mongod.exe':
-            os.popen('taskkill.exe /pid:' + str(pid) + ' -f')
+    pid = os.popen("tasklist | grep mongod | awk '{print $2}'").read()[:-1]
+    print(pid)
+    if pid:
+        os.popen('taskkill /pid {} /f'.format(pid))
+        print('kill mongo over')
+    else:
+        print('no mongo')
+
+
+def start_mongo_spider():
+    os.popen('mongod --dbpath=f:\data\db')
+    os.popen('python spider.py')
+
+
+def judge_spider(spider_pid):
+    p = psutil.Process(spider_pid)
+    memory_percent = psutil.virtual_memory().percent
+    pid_memory_percent = p.memory_percent()
+    print('memory_percent: ' + str(memory_percent), 'pid_memory_percent: ' + str(pid_memory_percent))
+    if memory_percent > int(default_memory_percent):
+        kill_spider(spider_pid)
+        kill_mongo()
+        print('restart spider and mongo')
+        time.sleep(10)
+        main()
+    else:
+        time.sleep(180)
+        main()
+
+
+def main():
+    start_mongo()
+    start_spider()
+    guardian_pwd = str(os.getpid())
+    pids = os.popen("tasklist | grep python | awk '{print $2}'").read()
+    pids = pids.split('\n')[:-1]
+    if guardian_pwd in pids:
+        pids.remove(guardian_pwd)
+    if not pids:
+        print('no pid, please start spider process')
+        return
+    spider_pid = pids[0]
+    print(spider_pid)
+    time.sleep(180)
+    judge_spider(int(spider_pid))
 
 
 if __name__ == '__main__':
-    os.system('mongod --dbpath=F:\data\db')
-    # p1 = multiprocessing.Process(target=start_mongo, args=())
-    # p1.start()
-    # time.sleep(3)
-    print('成功开启 mongodb')
-    # p2 = multiprocessing.Process(target=start_spider, args=())
-    # p2.start()
-    os.system('python spider.py')
-    print(os.getpid())
-    # print(p2.pid)
-    for pid in all_pid:
-        p = psutil.Process(pid)
-        if p.name() == 'python.exe':
-            print(p.name())
-            # judge_spider()
+    default_memory_percent = sys.argv[1]
+    main()
